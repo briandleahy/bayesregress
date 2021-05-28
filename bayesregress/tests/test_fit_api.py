e0af8b5aad424357fdda3a785362038c3ab98ecc
Brian Leahy <bdl48@cornell.edu>
2021-05-28 16:10:04 -0400

    test_fit_api

---
+++ bayesregress/tests/test_fit_api.py
@@ -0,0 +1,28 @@
+import unittest
+
+import numpy as np
+
+from bayesregress.regress import fit_data
+
+
+TOLS = {'atol': 1e-13, 'rtol': 1e-13}
+MEDTOLS = {"atol": 1e-6, "rtol": 1e-6}
+
+
+class TestDocs(unittest.TestCase):
+    def test_least_squares_regression(self):
+        rng = np.random.default_rng(seed=1516)
+
+        x = np.linspace(-10, 10, 901).reshape(-1, 1)
+        noise = rng.standard_normal(x.shape[0])
+        y = 0.5 * x.squeeze() * np.cos(0.5 * x.squeeze()) + noise
+
+        result = fit_data(x, y)
+
+        prediction = result.predict_for_map_model(x)
+        residuals = y - prediction
+        self.assertLessEqual(residuals.std(), 1.1 * noise.std())
+
+
+if __name__ == '__main__':
+    unittest.main()
